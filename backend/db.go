66610a51af8f4860d1cab95c92c4446995b6e3a2
yahya <muhyahyaas@gmail.com>
2025-07-05 20:22:13 +0700

    database selesai

---
+++ backend/db.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func Connect() {
+    var err error
+    DB, err = sql.Open("postgres", "postgres://postgres:deya2501@127.0.0.1:5432/timebanking_db?sslmode=disable")
+    if err != nil {
+        log.Fatal("Database connection error:", err)
+    }
+
+    err = DB.Ping()
+    if err != nil {
+        log.Fatal("Cannot ping DB:", err)
+    }
+
+    log.Println("Connected to PostgreSQL!")
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module timebanking
+
+go 1.24.3
